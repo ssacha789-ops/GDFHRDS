92008850075b58e22894bb62d5f5e921625f633e
ssacha789-ops <ssacha789@gmail.com>
2026-05-23 00:33:46 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import App from '@/App';
+import App from '../components/App'; // ou le chemin exact où se trouve ton fichier App
 
 export const metadata = {
   title: 'révise.',
